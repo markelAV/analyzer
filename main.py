f628365c8e2ab60201239ab11405285535352ade
artem <markelov1999180@gmail.com>
2018-11-09 19:36:09 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from venv import analyzer
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+
+
+class Root(BoxLayout):
+
+    input = ObjectProperty()
+    output = ObjectProperty()
+    anlyz = analyzer.Analyzer()
+
+    def out(self):
+        print(self.input.text)
+        s = self.input.text
+        lines = s.split('\n')
+        kod = []
+        i = 0
+        while i < len(lines) and len(lines) > 1:
+            if len(lines[i]) != 0:
+                kod.append(lines[i])
+            i += 1
+        b = self.anlyz.main_analyzer_method(kod)
+        if not b:
+            self.output.text = "Good"
+        else:
+            self.output.text = "Bad"
+
+
+
+class CustomLayout(FloatLayout):
+    def out(self):
+
+        self.output_Error.text = str("Error")
+
+
+class AnalyserApp(App):
+    def build(self):
+        root = Root()
+        return root
+
+    def testing(self):
+        print("Hello world")
+        anlyz = analyzer.Analyzer()
+        # anlyz.analyze_code()
+        numbers = {"-32768", "-32767", "32767", "32766", "-123", "123", "38213", "23h21", "7", "123456432"}
+        identifier = {"i", "J", "Tsdf", "dLKJs", "2df", "d3", "df.a"}
+        line1 = "FOR J12 = 85 TO 100 "
+        line2 = "FOR I =1 TO 10 STEP 2"
+        line3 = "FOR BL = 1 TO 10 STEP -2"
+        line1e = "FOR I =1 TO 10 STEP "
+        _2line1 = "A = 18 * A"
+        _2lline2 = "h = -386  + AX * 5 + ty"
+        _2line3 = "Z8 = 0"
+        lines = {line1, line2, line3, line1e}  # bag with line3!!!!!
+        _2lines = {_2line1, _2lline2, _2line3}
+        for i_line in _2lines:
+            if not anlyz.parse_second_line(i_line):
+                print(i_line, "  is ok")
+            else:
+                print(i_line, "  is not ok")
+        exit = "EXIT FOR "
+        nextq = " NEXT   R1"
+        t = len(exit)
+        if not anlyz.parse_fourth_line(nextq):
+            print(nextq, " - is ok")
+        else:
+            print(nextq, " - is not ok")
+        mline = [line2, _2line1, exit, nextq]
+        if not anlyz.main_analyzer_method(mline):
+            print("good")
+        else:
+            print("bad")
+
+if __name__ == '__main__':
+    AnalyserApp().run()
+    '''anlyz = analyzer.Analyzer()
+
+    numbers = {"-32768", "-32767", "32767", "32766", "-123", "123", "38213", "23h21", "7", "123456432"}
+    identifier = {"i", "J", "Tsdf", "dLKJs", "2df", "d3", "df.a"}
+    line1 = "FOR J12 = 85 TO 100 "
+    line2 = "FOR I =1 TO 10 STEP 2"
+    line3 = "FOR BL = 1 TO 10 STEP -2"
+    line1e = "FOR I =1 TO 10 STEP "
+    _2line1 = "A = 18 * A"
+    _2lline2 = "h = -386  + AX * 5 + ty"
+    _2line3 = "Z8 = 0"
+    lines = {line1, line2, line3, line1e}  # bag with line3!!!!!
+    _2lines = {_2line1, _2lline2, _2line3}
+    exit = "EXIT FOR "
+    nextq = " NEXT   I"
+    mline = [line2, _2line1, exit, nextq]
+    if not anlyz.main_analyzer_method(mline):
+        print("good")
+        s = "Indedificators: "
+        s += str(anlyz.inds)
+        print(s)
+    else:
+        print("bad")
+'''
---
+++ venv/analyzer.py
@@ -0,0 +1,245 @@
+
+
+# str[i].isdigit() - проверка строки на цифры
+# str[i].isalnum() - проверка строки на буквы и цифры
+# str[i].isalnum() - проверка строки на буквы
+'''         Описаниеи ошибок:
+    1. Ошибки в написании ключевых слов
+        1) Наличие недопустимых символов
+    2. Ошибки индентификатора:
+        1) Не начинается с буквы
+        2) Имеет длину более 8 символов
+        3) Является зарезервированным словом : FOR, TO, STEP , EXIT , NEXT.
+        4) Наличие недопусттимых символов
+    3. Ошибки констант:
+        1) Выход числа из диапозона -32768 - 32767
+        2) Наличие недопустимых символов
+    4. Наличие лишних символов в строке
+'''
+
+class Analyzer:
+
+    def __init__(self):
+        self.key_words = {"for", "to", "step", "exit", "next"}
+        self.inds = []
+        self.consts = []
+        self.error_message = []
+
+     # TESTING and Make and screw the TABLE
+
+    def control_const(self, _str, index):
+        if len(_str) == 0:
+            return -1
+        i = 0
+        min_side = -32768
+        max_side = 32767
+        flag_error = True
+        str_number = ""
+        minus = False
+        count_symbol = 0
+        max_symbol = 5
+        number = 0
+        if _str[i] == '-':
+            minus = True
+            i += 1
+
+        while (i < len(_str)) and (count_symbol <= max_symbol+1) and _str[i].isdigit():
+            str_number += _str[i]
+            count_symbol += 1
+            i += 1
+        if i == len(_str)or((count_symbol <= 5) and (_str[i] == ' ' or _str[i] == '\t')):
+            number = int(str_number)
+            if minus:
+                number = -number
+
+            if (number > min_side) and (number < max_side):
+                flag_error = False
+        if not flag_error:
+            #занесение константы в таблицу
+            #return number
+            self.consts.append(number) # deleate
+            index += i
+            return index
+        else:
+            return -1
+
+    # Testing, to redo last "if" and screw thr Table
+    def control_indentifier(self, _str, index):
+        i = 0
+        flag_error = True
+        max_symbol = 8
+        count_symbol = 0
+        indentifier = ""
+        # S.isalnum()	Состоит ли строка из цифр или букв проверить
+        if _str[i].isalpha():
+            indentifier += _str[i]
+            i += 1
+            count_symbol += 1
+            while i < len(_str) and count_symbol <= max_symbol+1 and _str[i].isalnum():
+                indentifier += _str[i]
+                i += 1
+                count_symbol += 1
+            if i == len(_str) or (count_symbol <= max_symbol and (_str[i] == ' ' or _str[i] == '\t')):
+                if(indentifier.lower() != "for") and (indentifier.lower() != "to") and\
+                        (indentifier.lower() != "exit") and (indentifier.lower() != "step") and\
+                        (indentifier.lower() != "next"):
+                    # уменьшить строку
+                    #действия по занесению в таблицу ( массив)
+                    flag_error = False
+        if not flag_error:
+            index += i
+            self.inds.append(indentifier)
+            return index
+        else:
+            return -1
+
+    #Tesing
+    #rename parse/ parsing
+
+    def parse_first_line(self, _str):
+        flags_error = True
+        _str = _str.lower()
+        i = 0
+        while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 12:
+            i += 1
+        if (len(_str) - i) >= 12 and _str.find("for", i, i+2):
+            i += 3
+            while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 8: # maybe not 8
+                i += 1
+            i = self.control_indentifier(_str[i:len(_str)], i)
+            if (len(_str) - i) >= 12 and i > 0:
+                while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 7: # maybe not 7
+                    i += 1
+                if (len(_str) - i) >= 7 and _str[i] == '=':
+                    i += 1
+                    while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 7:
+                        i += 1
+                    i = self.control_const(_str[i:len(_str)], i)
+                    if(len(_str) - i) >= 6 and i > 0:
+                        while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 4:
+                            i += 1
+                        if (len(_str) - i) >= 3 and _str.find("to", i, i+1):
+                            i += 2
+                            while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 2:
+                                i += 1
+                            i = self.control_const(_str[i:len(_str)], i)
+                            if i > 0:
+                                while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t'):
+                                    i += 1
+                                if i == len(_str):
+                                    flags_error = False
+                                else:
+                                    if (len(_str) - i) >= 5 and _str.find("step", i, i + 3):
+                                        i += 4
+                                        while (len(_str) - i) >= 1 and (_str[i] == ' ' or _str[i] == '\t'):
+                                            i += 1
+                                        i = self.control_const(_str[i:len(_str)], i)
+                                        if i > 0:
+                                            if i == len(_str):
+                                                flags_error = False
+                                            while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t'):
+                                                i += 1
+                                            if i == len(_str):
+                                                flags_error = False
+        return flags_error
+
+    def parse_second_line(self, _str):
+        flag_error = True
+        _str = _str.lower()
+        i = 0
+        while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 3:
+            i += 1
+        i = self.control_indentifier(_str[i:len(_str)], i)
+        if (len(_str) - i) >= 3 and i > 0:
+            while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 2:
+                i += 1
+            if (len(_str) - i) >= 2 and _str[i] == '=':
+                i += 1
+                if (len(_str) - i) >= 1:
+                    while i < len(_str):
+                        while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t') and (len(_str) - i) >= 1:
+                            i += 1
+                        i = self.parse_term(_str[i:len(_str)], i)
+                        if i > 0:
+                            while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t'):
+                                i += 1
+                            # if i == len(str):
+                            #    flag_error = False
+                            if i < len(_str) and (_str[i] == '-' or _str[i] == '+' or _str[i] == '*' or _str[i] == '/'):
+                                i += 1
+                        else:
+                            break
+                        if (i > 0) and (i == len(_str)):
+                            flag_error = False
+        return flag_error
+
+    def parse_term(self, _str, index):
+        i = 0
+        flag_error = True
+        while i < len(_str) and (_str[i] == ' ' or _str[i] == '\t'):
+            i += 1
+        if i < len(_str) and (_str[i].isalnum() or _str[i] == '-'):
+            if _str[i].isdigit() or _str[i] == '-':
+                i = self.control_const(_str[i:len(_str)], i)
+
+            else:
+                i = self.control_indentifier(_str[i:len(_str)], i)
+            if (i > 0) and (i <= len(_str)):
+                flag_error = False
+        if not flag_error:
+            index += i
+            return index
+        else:
+            return -1
+
+    def parse_third_line(self, _str):
+        flag_error = True
+        _str = _str.lower()
+        i = 0
+        while (i < len(_str)) and (_str[i] == ' ' or _str[i] == '\t'):
+            i += 1
+        if (len(_str) - i) >= 8 and ("exit " in _str):
+            i += 4
+            while (len(_str) - i) >= 3 and (_str[i] == ' ' or _str[i] == '\t'):
+                i += 1
+            if ((len(_str) - i) == 3 and ("for" in _str)) or ((len(_str) - i) > 3 and ("for " in _str)):
+                i += 3
+                while (i < len(_str)) and (_str[i] == ' ' or _str[i] == '\t'):
+                    i += 1
+                if i == len(_str):
+                    flag_error = False
+        return flag_error
+
+    def parse_fourth_line(self, _str):
+        _str = _str.lower()
+        flag_error = True
+        i = 0
+        while (i < len(_str)) and (_str[i] == ' ' or _str[i] == '\t'):
+            i += 1
+        if (len(_str) - i) >= 6 and ("next " in _str):
+            i += 4
+            while (i < len(_str)) and (_str[i] == ' ' or _str[i] == '\t'):
+                i += 1
+            if i < len(_str):
+                i = self.control_indentifier(_str[i:len(_str)], i)
+                if i > 0:
+                    while (i < len(_str)) and (_str[i] == ' ' or _str[i] == '\t'):
+                        i += 1
+                    if i == len(_str):
+                        flag_error = False
+        return flag_error
+
+    def main_analyzer_method(self, lines):
+        flag_error = True
+        self.inds = []
+        if len(lines) == 4 or len(lines) == 3:
+            if not self.parse_first_line(lines[0]) and not self.parse_second_line(lines[1]):
+                if len(lines)== 3 and not self.parse_fourth_line(lines[2]):
+                    flag_error = False
+                else:
+                    if not self.parse_third_line(lines[2]) and not self.parse_fourth_line(lines[3]):
+                        flag_error = False
+            if self.inds == [] or self.inds[0] != self.inds[len(self.inds)-1]:
+                flag_error = True
+        return flag_error
+
